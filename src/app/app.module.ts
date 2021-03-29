a2fd2ec97c80bed94d42e7075f9e3ffdc14dd1a9
efridge <efridge@uwf.edu>
2021-03-29 18:45:17 -0500

    Created three components on home page.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AddContactComponent } from './contact/add-contact/add-contact.component';
+import { ListContactsComponent } from './contact/list-contacts/list-contacts.component';
+import { ContactInfoComponent } from './contact/contact-info/contact-info.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent,
+    AddContactComponent,
+    ListContactsComponent,
+    ContactInfoComponent
   ],
   imports: [
     BrowserModule
