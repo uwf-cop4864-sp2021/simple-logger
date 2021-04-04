390122030de5b112f23e05fb55f95111bf4bca28
efridge <efridge@uwf.edu>
2021-04-04 11:19:34 -0500

    Integrated router with navbar.

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router, UrlSegment } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  public currentRoute = '';
+
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+    // Listen for events coming from the router
+    this.router.events.pipe(
+      // Only pay attention to navigation end events
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe((event: NavigationEnd) => {
+      this.currentRoute = event.url;
+    });
+  }
+
+  ngOnDestroy(): void {
+
   }
 
 }
